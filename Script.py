32ed23e55f02b3d31927e28c3ade7bf36720723b
Prime5522 <sahidhasannayem@gmail.com>
2024-07-16 12:39:25 +0600

    Update Script.py

--- Script.py
+++ Script.py
@@ -229,7 +229,7 @@ Nᴀᴍᴇ - {}"""
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
     CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {file_name}
-
+    <b> Upload by ⚙️: {@Prime_Movies4U}</b>
 <b> Size ⚙️: {file_size}</b>""" 
 
     IMDB_TEMPLATE_TXT = """
