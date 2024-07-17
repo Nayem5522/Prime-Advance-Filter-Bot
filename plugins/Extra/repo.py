c664c9f51389a083c0998aac3fffe005798a9cff
Prime5522 <sahidhasannayem@gmail.com>
2024-07-17 13:32:48 +0530

    Update repo.py

--- plugins/Extra/repo.py
+++ plugins/Extra/repo.py
@@ -35,7 +35,7 @@ async def git(bot, message):
 
 <b>Open Issues :</b> <i>{qw.get("open_issues")}</i>
 
-<b>Powered by :</b> @VJ_Botz
+<b>Powered by :</b> @Prime_Movies4U
 
 """
         if qw.get("description"):
