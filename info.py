811054799dbd4f9212335dc4f928543a7d21306d
Prime5522 <sahidhasannayem@gmail.com>
2024-07-17 19:38:27 +0600

    Update info.py

--- info.py
+++ info.py
@@ -71,8 +71,8 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'prime_Movies4u00') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+yRq4k6J8o2E0OWJl')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Movie_Request_Group4U')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+OzcPK0fNNew5Yjk1')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+OzcPK0fNNew5Yjk1')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/Prime_Movie_Watch_Dawnload/60')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Prime_Movies4U') # Support Chat Link Without https:// or @
 
