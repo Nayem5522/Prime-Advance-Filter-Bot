c7394403882a30c0a733b027887db63438c0925f
Prime5522 <sahidhasannayem@gmail.com>
2024-07-28 17:15:28 +0600

    Update info.py

--- info.py
+++ info.py
@@ -78,8 +78,8 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '+OzcPK0fNNew5Yjk1') # Support Chat L
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
-PM_SEARCH = bool(environ.get('PM_SEARCH', False))
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
+PM_SEARCH = bool(environ.get('PM_SEARCH', True))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
