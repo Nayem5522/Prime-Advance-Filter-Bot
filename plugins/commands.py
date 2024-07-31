4c602447125da4862f2b7e7da24b4f9bce74c03a
Prime5522 <sahidhasannayem@gmail.com>
2024-07-31 10:56:07 +0600

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -570,10 +570,10 @@ async def start(client, message):
             file = getattr(msg, filetype.value)
             title = '@Prime_Movies4U  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), file.file_name.split()))
             size=get_size(file.file_size)
-            f_caption = f"<code>{title}Join Now @Prime_Movies4U</code>"
+            f_caption = f"<code>{title}</code>"
             if CUSTOM_FILE_CAPTION:
                 try:
-                    f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='')
+                    f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='Join Now @Prime_Movies4U')
                 except:
                     return
             await msg.edit_caption(
