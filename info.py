1783de92089189c3572ea2809dcf589a1a798dd5
Prime5522 <sahidhasannayem@gmail.com>
2024-08-18 19:09:40 +0600

    Update info.py

--- info.py
+++ info.py
@@ -107,8 +107,8 @@ VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/Prime_Movie_Watch
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'droplink.co')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'd1e52488bac3d8297d89f895ed8ec64fd04253f8')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'teraboxbd.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'ccd0cdb45fb7e56ff8b3661033c9247478218b72')
 
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "5")
