f012cb74fb26c4de0cecd6c5b377ac0372fa3820
Nayem5522 <sahidhasannayembd@gmail.com>
2024-09-10 10:41:15 +0600

    Update approve.py

--- plugins/Extra/auto_approve/approve.py
+++ plugins/Extra/auto_approve/approve.py
@@ -172,6 +172,8 @@ async def auto_approve(client, message: ChatJoinRequest):
                             InlineKeyboardButton("🔥 Verify 🔥", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
                         ],[
                             InlineKeyboardButton("✅How To Open Link & Verify✅", url=VERIFY_TUTORIAL)
+                        ],[
+                            InlineKeyboardButton("✅How To Open Link & Nayem ✅", url="t.me/Prime_Nayem")
                         ]]
                         await client.send_message(
                             chat_id=message.from_user.id,
@@ -241,6 +243,8 @@ async def auto_approve(client, message: ChatJoinRequest):
                             InlineKeyboardButton("🔥 Verify 🔥", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
                         ],[
                             InlineKeyboardButton("✅How To Open Link & Verify✅", url=VERIFY_TUTORIAL)
+                        ],[
+                            InlineKeyboardButton("✅How To Open Link & Nayem ✅", url="t.me/Prime_Nayem")
                         ]]
                         await client.send_message(
                             chat_id=message.from_user.id,
@@ -312,9 +316,11 @@ async def auto_approve(client, message: ChatJoinRequest):
             if not await check_verification(client, message.from_user.id) and VERIFY == True:
                 btn = [[
                     InlineKeyboardButton("🔥 Verify 🔥", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
-                ],[
+                 ],[
                     InlineKeyboardButton("✅How To Open Link & Verify✅", url=VERIFY_TUTORIAL)
-                ]]
+                 ],[
+                    InlineKeyboardButton("✅How To Open Link & Nayem ✅", url="t.me/Prime_Nayem")
+                        ]]
                 await client.send_message(
                     chat_id=message.from_user.id,
                     text="<b>You are not verified !\nKindly verify to continue !\n\nJust Verify One Time And Get \nMovies For next 24hr without any \nverification (Ad)\n\nশুধু একবার verify করুন এবং পরবর্তী !\n24 ঘন্টার জন্য কোনো Ad ছাড়াই সিনেমা পান \n\nverify 🔥 এ ক্লিক করুন verified করতে \n\n(How to Open Link & verify✅ এ‌ ক্লিক করে কিভাবে verify করবেন তা দেখে নিন)\nClick The Button Below To Check How to Open Link & Verify✅ See verified tutorial</b>",
