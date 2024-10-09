ed93dc3ecd2d7188745765b60719a8ee0b59a3b4
Nayem5522 <sahidhasannayembd@gmail.com>
2024-10-09 12:01:41 +0600

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -717,7 +717,7 @@ async def log_file(bot, message):
 
 @Client.on_message(filters.command('delete') & filters.user(ADMINS))
 async def delete(bot, message):
-    reply = await bot.ask(message.from_user.id "Now Send Me Media Which You Want to delete")
+    reply = await bot.ask(message.from_user.id, "Now Send Me Media Which You Want to delete")
     if reply.media:
         msg = await message.reply("Processing...⏳", quote=True)
     else:
