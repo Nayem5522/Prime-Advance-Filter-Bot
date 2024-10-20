8340e6c396033e70efe80caadcec3587dd22f021
Nayem5522 <sahidhasannayembd@gmail.com>
2024-10-20 13:51:00 +0600

    Update info.py

--- info.py
+++ info.py
@@ -40,6 +40,7 @@ AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 # if REQUEST_TO_JOIN_MODE is true then force subscribe work like request to join fsub, else if false then work like normal fsub.
 REQUEST_TO_JOIN_MODE = bool(environ.get('REQUEST_TO_JOIN_MODE', True)) # Set True Or False
 TRY_AGAIN_BTN = bool(environ.get('TRY_AGAIN_BTN', True)) # Set True Or False (This try again button is only for request to join fsub not for normal fsub)
+REQ_CHANNEL = int(environ.get('REQ_CHANNEL', -1002154076690))
 auth_channel = environ.get('AUTH_CHANNEL', '-1002043502363') # give your force subscribe channel id here else leave it blank
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002242875858')
